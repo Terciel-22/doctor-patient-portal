f7c01b86bc04177d0bb8a885aa5603fba1964f56
Terciel-22 <despicablehaythamkenway@gmail.com>
2023-03-28 15:12:45 +0800

    Created Multiple Authentication

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+               'name' => 'Terciel Kenway',
+               'email' => 'tercielkenway@gmail.com',
+               'role' => 1,
+               'password' => bcrypt('123456'),
+            ],
+            [
+               'name' => 'Angelita Jamito',
+               'email' => 'angelitajamito@gmail.com',
+               'role' => 0,
+               'password' => bcrypt('123456'),
+            ],
+        ];
+    
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
